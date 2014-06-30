3648717ebee1de484d622542497f54123277ef40
ojdo <johannes.dorfner@ojdo.de>
2014-07-01 01:08:40 +0200

    cleaned and commented runme.py

--- runme.py
+++ runme.py
@@ -8,12 +8,17 @@ timesteps = np.arange(0, 24+1)
 
 model = urbs.create_model(filename, timesteps)
 prob = model.create()
-optim = SolverFactory('gurobi')  # 'cplex', 'gurobi', ...
+optim = SolverFactory('glpk')  # cplex, glpk, gurobi, ...
 result = optim.solve(prob, tee=True)
 prob.load(result)
 
+# read results to DataFrames for quick analyses
+(costs, cpro, ctra, csto, co2) = urbs.get_constants(prob)
+(created, consumed, stored, 
+ imported, exported) = urbs.get_timeseries(prob, 'Elec', 'DE')
+
+# write concise report to spreadsheet
 urbs.report(prob, 'report.xlsx', ['Elec'], ['DE', 'MA', 'NO'])
-# urbs.get_constants
-# urbs.get_timeseries
-# urbs.plot
-# urbs.report
+
+# create timeseries plot
+urbs.plot(prob, 'Elec', 'DE')

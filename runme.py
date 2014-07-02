57385b028bb9f82528b9e0827e0de83f8169d240
Johannes Dorfner <johannes.dorfner@tum.de>
2014-07-02 11:27:16 +0200

    added MA process cap-up's to data-example.xlsx, colour customization in runme.py

--- runme.py
+++ runme.py
@@ -3,23 +3,34 @@ import pandas as pd
 import urbs
 from coopr.opt.base import SolverFactory
 
+# input data
 filename = 'data-example.xlsx'
-(offset, length) = (4000, 15*24)
+
+# settings
+(offset, length) = (4000, 5*24)  # timestep selection
 timesteps = np.arange(offset, offset+length+1)
 
+#
 model = urbs.create_model(filename, timesteps)
 prob = model.create()
 optim = SolverFactory('glpk')  # cplex, glpk, gurobi, ...
 result = optim.solve(prob, tee=True)
 prob.load(result)
 
-# read results to DataFrames for quick analyses
+# read results (only for interactive use)
 (costs, cpro, ctra, csto, co2) = urbs.get_constants(prob)
 (created, consumed, stored, 
  imported, exported) = urbs.get_timeseries(prob, 'Elec', 'DE')
 
 # write concise report to spreadsheet
 urbs.report(prob, 'report.xlsx', ['Elec'], ['DE', 'MA', 'NO'])
+
+# add or change plot colours
+for country, colour in {
+        'DE': (220, 200, 200), 
+        'MA': (200, 220, 200),
+        'NO': (200, 200, 220)}.iteritems():
+   urbs.COLOURS[country] = colour 
 
 # create timeseries plot
 for sit in ['DE', 'MA', 'NO']:

36b6610295aebe21f43c5f8360b7d1cdf26965d5
ojdo <johannes.dorfner@ojdo.de>
2014-07-31 16:43:36 +0200

    docstrings for timeseries

--- urbs.py
+++ urbs.py
@@ -27,16 +27,19 @@ Model entities
 Commodity
 ---------
 
-Commodities can have one of four types:
-  - Stock: Buyable at any time for a given price. Supply can be limited
-    per timestep or for a whole year. Examples are coal, gas, uranium
-    or biomass.
-  - SupIm: Supply intermittent stands for fluctuating resources like
+Commodities are goods that can be generated, stored, transmitted and consumed.
+By convention, they are represented by their energy content (in MWh), but can
+be changed (to J, kW, t, kg) by simply using different (consistent) units for
+all input data. Each commodity must be exactly one of following four types:
+  - Demand: The 
+  - Stock: Can be introduced (bought, mined, taken out of "stock") in arbitrary 
+    quantities at any time for a given, fixed price per unit. Its maximum 
+    supply can be limited per timestep or for a whole year. Typical examples of
+    stock commodities are coal, gas, uranium or biomass.
+  - SupIm: Fluctuating resources like
     solar radiation and wind energy, which are available according to
-    a timeseries of values, which could be derived from weather data.
-  - Demand: These commodities have a timeseries for the requirement
-    associated and must be provided by output from other processes or
-    from storage.
+    a timeseries of values. Can also be used to model "must-run" plants like
+    uncontrollable, distributed generation units.
   - Env: The special commodity CO2 is of this type and represents the
     amount (in tons) of greenhouse gas emissions from processes. Its
     total amount can be limited, to investigate the effect of policies
@@ -48,7 +51,9 @@ a maximum allowed quantity that may be created.
 
 Process
 -------
-
+Processes describe conversion technologies from one commodity to another. They
+can be visualised like a black box with one input (commodity) and one output
+(commodity). A fixed conversion efficiency 
 
 
 Transmission
@@ -57,14 +62,26 @@ Transmission
 Storage
 -------
 
+
 Timeseries
 ==========
 
 Demand
 ------
+Each combination of site and demand commodity may have one timeseries,
+describing the (average) power demand (MWh/h) per timestep. They are a crucial
+input parameter, as the whole optimisation aims to satisfy these demands with
+minimal costs from the given technologies (process, storage, transmission).
 
 Intermittent Supply
 -------------------
+Each combination (site, supim commodity) must be supplied with one timeseries,
+normalised to a maximum value of 1 relative to the installed capacity of a
+process using this commodity as input. For eample, a wind power timeseries
+should reach value 1, when the wind speed exceeds the modelled wind turbine's
+design wind speed is exceeded. This implies that any non-linear behaviour of
+intermittent processes can already be incorporated while preparing this
+timeseries.
 
 
 """

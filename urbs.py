1c55c051fa7db6b15a5b5ef11ba93cc2efbba950
Thomas Zipperle <thomas.zipperle@tum.de>
2015-06-10 11:17:32 +0200

    Code refactoring: time-dependent commodity prices #3

--- urbs.py
+++ urbs.py
@@ -725,21 +725,12 @@ def res_sell_buy_symmetry_rule(m, sit_in, pro_in, coin):
 # constraint only for sell and buy processes
 # and the processes musst be in the same site
     if coin in m.com_buy:
-        pro_output_tuples = list(m.pro_output_tuples.value)
-        pro_input_tuples = list(m.pro_input_tuples.value)
-        # search the output commodities for the "buy" process
-        # out = (site,output_commodity)
-        out = set([(x[0],x[2]) for x in pro_output_tuples if x[1] == pro_in])
-        # search the sell process for the output_commodity from the buy process
-        inp_p = ([x for x in pro_output_tuples if x[2] in m.com_sell])
-        for k in range(len(inp_p)):
-            pro_out = inp_p[k][1]
-            inp = set([(x[0],x[2]) for x in pro_input_tuples if x[1] == pro_out])
-            # check: buy - commodity == commodity - sell; for a site
-            if not(inp.isdisjoint(out)):
-                return (m.cap_pro[sit_in, pro_in] ==
-                        m.cap_pro[sit_in, pro_out])
-        return pyomo.Constraint.Skip
+        sell_pro = search_sell_buy_tuple(m, sit_in, pro_in, coin)
+        if sell_pro is None:
+            return pyomo.Constraint.Skip
+        else:
+            return (m.cap_pro[sit_in, pro_in] ==
+                        m.cap_pro[sit_in, sell_pro])
     else:
         return pyomo.Constraint.Skip
 
@@ -908,42 +899,21 @@ def def_costs_rule(m, cost_type):
             if c[1] in m.com_stock)
 
     elif cost_type == 'Revenue':
-        revenue = 0
-        sell_tuples = commodity_subset_list(m.com_tuples, m.com_sell)
-        for c in sell_tuples:
-            # check com price fix or timeseries
-            if not isinstance(m.commodity.loc[c]['price'], float):
-                # factor, to realize a different commodity price for each site
-                factor = extract_number_str(m.commodity.loc[c]['price'])
-                # calculate the purchase with the hourly commodity price
-                revenue += sum(m.e_co_sell[(tm,) + c] * factor *
-                   m.buy_sell_price.loc[(tm,) + (c[1],)] * m.weight * m.dt
-                   for tm in m.tm)
-            else:
-                # commodity price is the same in each hour
-                revenue += sum(m.e_co_sell[(tm,) + c] * m.dt *
-                   m.commodity.loc[c]['price'] * m.weight
-                   for tm in m.tm)
-        return m.costs['Revenue'] == -revenue
+        sell_tuples = commodity_subset(m.com_tuples, m.com_sell)
+        com_prices = get_com_price(m, sell_tuples)
+
+        return m.costs['Revenue'] == -sum(
+            m.e_co_sell[(tm,) + c] * com_prices[c].loc[tm] * m.weight * m.dt
+            for tm in m.tm for c in sell_tuples)
 
     elif cost_type == 'Purchase':
-        purchase = 0
-        buy_tuples = commodity_subset_list(m.com_tuples, m.com_buy)
-        for c in buy_tuples:
-            # check com price fix or timeseries
-            if not isinstance(m.commodity.loc[c]['price'], float):
-                # factor, to realize a different commodity price for each site
-                factor = extract_number_str(m.commodity.loc[c]['price'])
-                # calculate the purchase with the hourly commodity price
-                purchase += sum(m.e_co_buy[(tm,) + c] * factor *
-                    m.buy_sell_price.loc[(tm,) + (c[1],)] * m.weight * m.dt
-                    for tm in m.tm)
-            else:
-                # commodity price is the same in each hour
-                purchase += sum(m.e_co_buy[(tm,) + c] * m.dt *
-                    m.commodity.loc[c]['price'] * m.weight
-                    for tm in m.tm)
-        return m.costs['Purchase'] == purchase
+        buy_tuples = commodity_subset(m.com_tuples, m.com_buy)
+        com_prices = get_com_price(m, buy_tuples)
+
+        return m.costs['Purchase'] == sum(
+            m.e_co_buy[(tm,) + c] * com_prices[c].loc[tm] * m.weight * m.dt
+            for tm in m.tm for c in buy_tuples)
+
     else:
         raise NotImplementedError("Unknown cost type.")
 
@@ -1088,40 +1058,54 @@ def commodity_subset(com_tuples, type_name):
 
     Args:
         com_tuples: a list of (site, commodity, commodity type) tuples
-        type_name: a commodity type ('Stock', 'SupIm', 'Env' or 'Demand')
+        type_name: a commodity type or a ist of a commodity types
 
     Returns:
-        The set (unique elements) of commodity names of the desired type
+        The set (unique elements/list) of commodity names of the desired type
     """
-    return set(com for sit, com, com_type in com_tuples 
-               if com_type == type_name)
+    if type(type_name) is str:
+        # type_name: ('Stock', 'SupIm', 'Env' or 'Demand')
+        return set(com for sit, com, com_type in com_tuples
+                   if com_type == type_name)
+    else:
+        # type(type_name) is a class 'coopr.pyomo.base.sets.SimpleSet'
+        # type_name: ('Buy')=>('Elec buy', 'Heat buy')
+        return set((sit, com, com_type) for sit, com, com_type in com_tuples
+                   if com in type_name)
 
-def commodity_subset_list(com_tuples, type_list):
-    """ Unique list of commodity names for given list (type).
+def get_com_price(instance, tuples):
+    """ Calculate commodity prices for each modelled timestep.
 
     Args:
-        com_tuples: a list of (site, commodity, commodity type) tuples
-        type_list: list of a commodity type ('Buy')=>('Elec buy', 'Heat buy')
+        instance: a Pyomo ConcreteModel instance
+        tuples: a list of (site, commodity, commodity type) tuples
 
     Returns:
-        The set (unique list of tuples) of tuples of the desired type
+        a Pandas DataFrame with entities as columns and timesteps as index
     """
-    return set((sit, com, com_type) for sit, com, com_type in com_tuples
-               if com in type_list)
+    com_price = pd.DataFrame(index=instance.tm)
+    for c in tuples:
+        # check commodity price: fix or has a timeseries
+        # type(instance.commodity.loc[c]['price']):
+        # float => fix: com price = 0.15
+        # string => var: com price = '1.25xBuy' (Buy: refers to timeseries)
+        if not isinstance(instance.commodity.loc[c]['price'], float):
+            # a different commodity price for each hour
+            # factor, to realize a different commodity price for each site
+            factor = extract_number_str(instance.commodity.loc[c]['price'])
+            price = factor * instance.buy_sell_price.loc[(instance.tm,) + (c[1],)]
+            com_price[c] = pd.Series(price, index=com_price.index)
+        else:
+            # same commdoity price for each hour
+            price = instance.commodity.loc[c]['price']
+            com_price[c] = pd.Series(price, index=com_price.index)
+    return com_price
 
 def extract_number_str(str_in):
-    """ Extract the first number from a given string and
-        and convert to the float type
+    """ Extract first number from a given string and convert to a float number.
 
-        Excel sheet: Commodity - price - for Buy and Sell:
-
-            - fix price: (0.12) [input type: float]
-                the same price for each timestep
-            - timeseries: (1.25BUY) [input type: string]
-                1.) extract the number 1.25
-                2.) select the com price for the given timestep
-                3.) calculate the price with the factor (1.25)
-                    => 1.25 x Buy-Sell-Price(h)
+    The function works with the following formats (,25), (.25), (2), (2,5), (2.5),
+    (1,000.25), (1.000,25) and  doesn't with (1e3), (1.5-0.4j) and negative numbers.
 
     Args:
         str_in: a string ('1,20BUY')
@@ -1130,14 +1114,65 @@ def extract_number_str(str_in):
         A float number (1.20)
     """
     import re
-    # replace all commas with points
-    str_in = str_in.replace(',','.')
-    # find the first number and convert to float
-    factor_str = re.search('\d+[\.]?\d*',str_in)
-    if factor_str is None:
-        return 1
-    else:
-        return float(factor_str.group(0))
+    # deletes all char starting after the number
+    start_char = re.search('[*:!%$&?a-zA-Z]',str_in).start()
+    str_num = str_in[:start_char]
+
+    if re.search('\d+',str_num) is None:
+        # no number in str_num
+        return 1.0
+    elif re.search('^(\d+|\d{1,3}(,\d{3})*)(\.\d+)?$',str_num) is not None:
+        #Commas required between powers of 1,000
+        #Can't start with "."
+        #Pass: (1,000,000), (0.001)
+        #Fail: (1000000), (1,00,00,00), (.001)
+        str_num = str_num.replace(',','')
+        return float(str_num)
+    elif re.search('^(\d+|\d{1,3}(.\d{3})*)(\,\d+)?$',str_num) is not None:
+        #Dots required between powers of 1.000
+        #Can't start with ","
+        #Pass: (1.000.000), (0,001)
+        #Fail: (1000000), (1.00.00,00), (,001)
+        str_num = str_num.replace('.','')
+        return float(str_num.replace(',','.'))
+    elif re.search('^\d*\.?\d+$',str_num) is not None:
+        #No commas allowed
+        #Pass: (1000.0), (001), (.001)
+        #Fail: (1,000.0
+        return float(str_num)
+    elif re.search('^\d*\,?\d+$',str_num) is not None:
+        #No dots allowed
+        #Pass: (1000,0), (001), (,001)
+        #Fail: (1.000,0
+        str_num = str_num.replace(',','.')
+        return float(str_num)
+
+def search_sell_buy_tuple(instance, sit_in, pro_in, coin):
+    """ Return the equivalent sell-process for a given buy-process.
+
+    Args:
+        instance: a Pyomo ConcreteModel instance
+        sit_in: a site
+        pro_in: a process
+        co_in: a commodity
+
+    Returns:
+        a process
+    """
+    pro_output_tuples = list(instance.pro_output_tuples.value)
+    pro_input_tuples = list(instance.pro_input_tuples.value)
+    # search the output commodities for the "buy" process
+    # buy_out = (site,output_commodity)
+    buy_out = set([(x[0],x[2]) for x in pro_output_tuples if x[1] == pro_in])
+    # search the sell process for the output_commodity from the buy process
+    sell_output_tuple = ([x for x in pro_output_tuples if x[2] in instance.com_sell])
+    for k in range(len(sell_output_tuple)):
+        sell_pro = sell_output_tuple[k][1]
+        sell_in = set([(x[0],x[2]) for x in pro_input_tuples if x[1] == sell_pro])
+        # check: buy - commodity == commodity - sell; for a site
+        if not(sell_in.isdisjoint(buy_out)):
+            return sell_pro
+    return None
 
 def get_entity(instance, name):
     """ Return a DataFrame for an entity in model instance.

7ff4592d30e546d6120ba0a87975815dea8df423
Konrad Schönleber <konrad.schoenleber@tum.de>
2018-07-04 12:34:59 +0200

    Functionality added: If no-value is given for initital state of storage, it is treated as a variable

--- urbs/input.py
+++ urbs/input.py
@@ -134,6 +134,10 @@ def pyomo_model_prep(data, timesteps):
     m.r_out_min_fraction = m.r_out_min_fraction['ratio-min']
     m.r_out_min_fraction = m.r_out_min_fraction[m.r_out_min_fraction > 0]
 
+    # storages with initial state as variable
+    m.stor_init_bound = m.storage['init']
+    m.stor_init_bound = m.stor_init_bound[m.stor_init_bound >= 0]
+
     # derive annuity factor from WACC and depreciation duration
     m.process['annuity-factor'] = annuity_factor(
         m.process['depreciation'],

3d1cca7a4e0c8864e31649e14122199d1fe7d406
Konrad Schönleber <konrad.schoenleber@tum.de>
2018-11-07 15:11:28 +0100

    Removed obsolete if __name__==main staement in  simplified runme.py

--- runme.py
+++ runme.py
@@ -7,70 +7,69 @@ from datetime import datetime
 from pyomo.opt.base import SolverFactory
 
 
-if __name__ == '__main__':
-    input_file = 'mimo-example.xlsx'
-    result_name = os.path.splitext(input_file)[0]  # cut away file extension
-    result_dir = urbs.prepare_result_directory(result_name)  # name+time stamp
+input_file = 'mimo-example.xlsx'
+result_name = os.path.splitext(input_file)[0]  # cut away file extension
+result_dir = urbs.prepare_result_directory(result_name)  # name+time stamp
 
-    # copy input file to result directory
-    shutil.copyfile(input_file, os.path.join(result_dir, input_file))
-    # copy runme.py to result directory
-    shutil.copy(__file__, result_dir)
-    
-    # Choose solver (cplex, glpk, gurobi, ...)
-    solver = 'glpk'
+# copy input file to result directory
+shutil.copyfile(input_file, os.path.join(result_dir, input_file))
+# copy runme.py to result directory
+shutil.copy(__file__, result_dir)
 
-    # simulation timesteps
-    (offset, length) = (3500, 168)  # time step selection
-    timesteps = range(offset, offset+length+1)
-    dt = 1  # length of each time step (unit: hours)
+# Choose solver (cplex, glpk, gurobi, ...)
+solver = 'gurobi'
 
-    # plotting commodities/sites
-    plot_tuples = [
-        ('North', 'Elec'),
-        ('Mid', 'Elec'),
-        ('South', 'Elec'),
-        (['North', 'Mid', 'South'], 'Elec')]
+# simulation timesteps
+(offset, length) = (3500, 168)  # time step selection
+timesteps = range(offset, offset+length+1)
+dt = 1  # length of each time step (unit: hours)
 
-    # optional: define names for sites in plot_tuples
-    plot_sites_name = {('North', 'Mid', 'South'): 'All'}
+# plotting commodities/sites
+plot_tuples = [
+    ('North', 'Elec'),
+    ('Mid', 'Elec'),
+    ('South', 'Elec'),
+    (['North', 'Mid', 'South'], 'Elec')]
 
-    # detailed reporting commodity/sites
-    report_tuples = [
-        ('North', 'Elec'), ('Mid', 'Elec'), ('South', 'Elec'),
-        ('North', 'CO2'), ('Mid', 'CO2'), ('South', 'CO2')]
+# optional: define names for sites in plot_tuples
+plot_sites_name = {('North', 'Mid', 'South'): 'All'}
 
-    # optional: define names for sites in report_tuples
-    report_sites_name = {'North': 'Greenland'}
+# detailed reporting commodity/sites
+report_tuples = [
+    ('North', 'Elec'), ('Mid', 'Elec'), ('South', 'Elec'),
+    ('North', 'CO2'), ('Mid', 'CO2'), ('South', 'CO2')]
 
-    # plotting timesteps
-    plot_periods = {
-        'all': timesteps[1:]
-    }
+# optional: define names for sites in report_tuples
+report_sites_name = {'North': 'Greenland'}
 
-    # add or change plot colors
-    my_colors = {
-        'South': (230, 200, 200),
-        'Mid': (200, 230, 200),
-        'North': (200, 200, 230)}
-    for country, color in my_colors.items():
-        urbs.COLORS[country] = color
+# plotting timesteps
+plot_periods = {
+    'all': timesteps[1:]
+}
 
-    # select scenarios to be run
-    scenarios = [
-        urbs.scenario_base,
-        urbs.scenario_stock_prices,
-        urbs.scenario_co2_limit,
-        urbs.scenario_co2_tax_mid,
-        urbs.scenario_no_dsm,
-        urbs.scenario_north_process_caps,
-        urbs.scenario_all_together]
+# add or change plot colors
+my_colors = {
+    'South': (230, 200, 200),
+    'Mid': (200, 230, 200),
+    'North': (200, 200, 230)}
+for country, color in my_colors.items():
+    urbs.COLORS[country] = color
 
-    for scenario in scenarios:
-        prob = urbs.run_scenario(input_file, solver, timesteps, scenario,
-                            result_dir, dt,
-                            plot_tuples=plot_tuples,
-                            plot_sites_name=plot_sites_name,
-                            plot_periods=plot_periods,
-                            report_tuples=report_tuples,
-                            report_sites_name=report_sites_name)
+# select scenarios to be run
+scenarios = [
+    urbs.scenario_base,
+    urbs.scenario_stock_prices,
+    urbs.scenario_co2_limit,
+    urbs.scenario_co2_tax_mid,
+    urbs.scenario_no_dsm,
+    urbs.scenario_north_process_caps,
+    urbs.scenario_all_together]
+
+for scenario in scenarios:
+    prob = urbs.run_scenario(input_file, solver, timesteps, scenario,
+                        result_dir, dt,
+                        plot_tuples=plot_tuples,
+                        plot_sites_name=plot_sites_name,
+                        plot_periods=plot_periods,
+                        report_tuples=report_tuples,
+                        report_sites_name=report_sites_name)

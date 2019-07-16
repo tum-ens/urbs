c5ac65b365a828b37b9d43e00170194f242a00bd
yunusozsahin <yunusozsahin@gmail.com>
2019-07-16 13:05:35 +0200

    transmission_cost switch case changed

--- urbs/features/transmission.py
+++ urbs/features/transmission.py
@@ -348,51 +348,34 @@ def transmission_balance(m, tm, stf, sit, com):
 # transmission cost function
 def transmission_cost(m, cost_type):
     """returns transmission cost function for the different cost types"""
-    if m.mode['dpf']:
-        if cost_type == 'Invest':
-            cost = sum(m.cap_tra_new[t] *
-                       m.transmission_dict['inv-cost'][t] *
-                       m.transmission_dict['invcost-factor'][t]
-                       for t in m.tra_tuples)
-            if m.mode['int']:
-                cost -= sum(m.cap_tra_new[t] *
-                            m.transmission_dict['inv-cost'][t] *
-                            m.transmission_dict['overpay-factor'][t]
-                            for t in m.tra_tuples)
-            return cost
-        elif cost_type == 'Fixed':
-            return sum(m.cap_tra[t] * m.transmission_dict['fix-cost'][t] *
-                       m.transmission_dict['cost_factor'][t]
-                       for t in m.tra_tuples)
-        elif cost_type == 'Variable':
+    if cost_type == 'Invest':
+        cost = sum(m.cap_tra_new[t] *
+                   m.transmission_dict['inv-cost'][t] *
+                   m.transmission_dict['invcost-factor'][t]
+                   for t in m.tra_tuples)
+        if m.mode['int']:
+            cost -= sum(m.cap_tra_new[t] *
+                        m.transmission_dict['inv-cost'][t] *
+                        m.transmission_dict['overpay-factor'][t]
+                        for t in m.tra_tuples)
+        return cost
+    elif cost_type == 'Fixed':
+        return sum(m.cap_tra[t] * m.transmission_dict['fix-cost'][t] *
+                   m.transmission_dict['cost_factor'][t]
+                   for t in m.tra_tuples)
+    elif cost_type == 'Variable':
+        if m.mode['dpf']:
             return sum(m.e_tra_in[(tm,) + t] * m.weight *
                        m.transmission_dict['var-cost'][t] *
                        m.transmission_dict['cost_factor'][t]
                        for tm in m.tm
-                       for t in m.tra_tuples_tp) + \
+                       for t in m.tra_tuples) + \
                    sum(m.abs_e_tra_dc[(tm,) + t] * m.weight *
                        m.transmission_dict['var-cost'][t] *
                        m.transmission_dict['cost_factor'][t]
                        for tm in m.tm
                        for t in m.tra_tuples_dc)
-    else:
-
-        if cost_type == 'Invest':
-            cost = sum(m.cap_tra_new[t] *
-                       m.transmission_dict['inv-cost'][t] *
-                       m.transmission_dict['invcost-factor'][t]
-                       for t in m.tra_tuples)
-            if m.mode['int']:
-                cost -= sum(m.cap_tra_new[t] *
-                            m.transmission_dict['inv-cost'][t] *
-                            m.transmission_dict['overpay-factor'][t]
-                            for t in m.tra_tuples)
-            return cost
-        elif cost_type == 'Fixed':
-            return sum(m.cap_tra[t] * m.transmission_dict['fix-cost'][t] *
-                       m.transmission_dict['cost_factor'][t]
-                       for t in m.tra_tuples)
-        elif cost_type == 'Variable':
+        else:
             return sum(m.e_tra_in[(tm,) + t] * m.weight *
                        m.transmission_dict['var-cost'][t] *
                        m.transmission_dict['cost_factor'][t]

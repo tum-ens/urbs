5f43a7e1ea43560e4f2a097495565400eaad006d
lodersky <lenhard.odersky@tum.de>
2019-11-29 17:46:10 +0100

    Fix for CO2 optimization * model.py: changing cost/co2 limit/budget definition * validation.py: check for cost/co2 limit/budget, add if missing * runfunctions.py: saving lp-file in result folder

--- urbs/runfunctions.py
+++ urbs/runfunctions.py
@@ -93,7 +93,8 @@ def run_scenario(input_files, Solver, timesteps, scenario, result_dir, dt,
 
     # create model
     prob = create_model(data, dt, timesteps, objective)
-    # prob.write('model.lp', io_options={'symbolic_solver_labels':True})
+    # prob_filename = os.path.join(result_dir, 'model.lp')
+    # prob.write(prob_filename, io_options={'symbolic_solver_labels':True})
 
     # refresh time stamp string and create filename for logfile
     log_filename = os.path.join(result_dir, '{}.log').format(sce)
--- urbs/validation.py
+++ urbs/validation.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 
 def validate_input(data):
     """ Input validation function
@@ -31,6 +31,24 @@ def validate_input(data):
                                  com + ') is not in commodity input sheet.'
                                  '! The pair (' + sit + ',' + com + ')'
                                  ' is not in commodity input sheet.')
+
+
+    # Add global parameters if necessary
+    for stf in data['global_prop'].index.get_level_values(0):
+        if 'Cost limit' not in data['global_prop'].loc[stf].index:
+            data['global_prop'].loc[(stf, 'Cost limit')] = np.inf
+            print('Added a global Cost limit for ' + str(stf) + ' with the value: inf.')
+        if 'CO2 limit' not in data['global_prop'].loc[stf].index:
+            data['global_prop'].loc[(stf, 'CO2 limit'), :] = np.inf
+            print('Added a global CO2 limit for ' + str(stf) + ' with the value: inf.')
+        if stf == min(data['global_prop'].index.get_level_values(0)):
+            if 'Cost budget' not in data['global_prop'].loc[stf].index:
+                data['global_prop'].loc[(stf, 'Cost budget'), :] = np.inf
+                print('Added a global Cost budget for the entire period with the value: inf.')
+            if 'CO2 budget' not in data['global_prop'].loc[stf].index:
+                data['global_prop'].loc[(stf, 'CO2 budget'), :] = np.inf
+                print('Added a global CO2 budget for the entire period with the value: inf.')
+
 
     # Find ducplicate index
     for key in data:
@@ -70,9 +88,11 @@ def validate_input(data):
                     if data['transmission'].loc[index]['eff'] != 1:
                         raise ValueError('Ensure efficiency of DCPF Transmission Lines are 1')
                     if not data['transmission'].loc[index]['base_voltage'] > 0:
-                        raise ValueError('Ensure base voltage of DCPF transmission lines are greater than 0')
+                        raise ValueError('Ensure base voltage of DCPF transmission lines are '
+                                         'greater than 0')
                     if not (0 < data['transmission'].loc[index]['difflimit'] <= 90):
-                        raise ValueError('Ensure angle difference of DCPF transmission lines are between 90 and 0 '
+                        raise ValueError('Ensure angle difference of DCPF transmission lines '
+                                         'are between 90 and 0 '
                                          'degrees')
 
     if not data['storage'].empty:
@@ -143,5 +163,7 @@ def validate_input(data):
 def validate_dc_objective(data, objective):
     if not data['transmission'].empty:
         if 'reactance' in data['transmission'].keys():
-            if any(data['transmission']['reactance'] > 0) and (objective == 'CO2') and any(data['transmission']['var-cost'] > 0):
-                print("\nif the C02 is selected as objective function while modelling DC transmission lines, variable costs may be incorrect \n")
+            if any(data['transmission']['reactance'] > 0) and (objective == 'CO2') \
+                    and any(data['transmission']['var-cost'] > 0):
+                print("\nif the C02 is selected as objective function while modelling "
+                      "DC transmission lines, variable costs may be incorrect \n")

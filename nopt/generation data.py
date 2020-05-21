29b565fd27b321ca41d5e5fd8685d0a31fc84757
kayaraski <kazkayasi.ilsu@gmail.com>
2020-05-21 18:21:27 +0200

    Supim completed

--- nopt/generation data.py
+++ nopt/generation data.py
@@ -2,33 +2,67 @@ import pandas as pd
 import numpy as np
 import datetime
 from datetime import datetime
-data= pd.read_csv(r'C:\Thesis\Input Resources\Excel\Actual_generation_201601010000_201612312359_1.csv',sep=';')
+data= pd.read_csv(r'C:\Thesis\Input Resources\Excel\DE_2019_generation.csv',sep=';')
+PV_2016= 40680
+Wind_on_2016=45280
+Wind_off_2016=4150
+Hydro_2016=5490
 
-data['Time']=data['Time of day'].apply(lambda x: x[:-3])
-data['Time']=data['Time'].apply(lambda x: datetime.strptime(x, '%H:%M').hour)
-#data['Date']=data['Date'].apply(lambda x: datetime.strptime(x, '%d.%m.%Y').date())
-data['Hydropower[MWh]']=data['Hydropower[MWh]'].map(lambda x : x.replace('.',''))
-data['Hydropower[MWh]']=data['Hydropower[MWh]'].map(lambda x: x.replace(',','.'))
-data['Hydropower[MWh]']= data['Hydropower[MWh]'].replace('-', np.nan)
-data['Hydropower[MWh]']= data['Hydropower[MWh]'].astype(float)
+PV_2017= 42290
+Wind_on_2017=50170
+Wind_off_2017=5410
+Hydro_2017=4780
 
-data['Wind offshore[MWh]']=data['Wind offshore[MWh]'].map(lambda x : x.replace('.',''))
-data['Wind offshore[MWh]']=data['Wind offshore[MWh]'].map(lambda x: x.replace(',','.'))
-data['Wind offshore[MWh]']= data['Wind offshore[MWh]'].replace('-', np.nan)
-data['Wind offshore[MWh]']= data['Wind offshore[MWh]'].astype(float)
+PV_2018=45230
+Wind_on_2018=52450
+Wind_off_2018=6400
+Hydro_2018=4780
 
-data['Wind onshore[MWh]']=data['Wind onshore[MWh]'].map(lambda x : x.replace('.',''))
-data['Wind onshore[MWh]']=data['Wind onshore[MWh]'].map(lambda x: x.replace(',','.'))
-data['Wind onshore[MWh]']= data['Wind onshore[MWh]'].replace('-', np.nan)
-data['Wind onshore[MWh]']= data['Wind onshore[MWh]'].astype(float)
+PV_2019=49180
+Wind_on_2019=53370
+Wind_off_2019=7500
+Hydro_2019=4780
 
-data['Photovoltaics[MWh]']=data['Photovoltaics[MWh]'].map(lambda x : x.replace('.',''))
-data['Photovoltaics[MWh]']=data['Photovoltaics[MWh]'].map(lambda x: x.replace(',','.'))
-data['Photovoltaics[MWh]']= data['Photovoltaics[MWh]'].replace('-', np.nan)
-data['Photovoltaics[MWh]']= data['Photovoltaics[MWh]'].astype(float)
+data['Time']=data['Time of day'].apply(lambda x: datetime.strptime(x, '%I:%M %p').hour)
+data['Date']=data['Date'].apply(lambda x: datetime.strptime(x, '%b %d, %Y').date())
+#data['Hydropower[MWh]']=data['Hydropower[MWh]'].map(lambda x : x.replace('.',''))
+#data['Hydropower[MWh]']=data['Hydropower[MWh]'].map(lambda x: x.replace(',','.'))
+if not data['Hydropower[MWh]'].dtype==float:
+    data['Hydropower[MWh]']=data['Hydropower[MWh]'].map(lambda x: x.replace(',',''))
+    data['Hydropower[MWh]']= data['Hydropower[MWh]'].replace('-', np.nan)
+    data['Hydropower[MWh]']= data['Hydropower[MWh]'].astype(float)
+
+#data['Wind offshore[MWh]']=data['Wind offshore[MWh]'].map(lambda x : x.replace('.',''))
+#data['Wind offshore[MWh]']=data['Wind offshore[MWh]'].map(lambda x: x.replace(',','.'))
+if not data['Wind offshore[MWh]'].dtype==float:
+    data['Wind offshore[MWh]']=data['Wind offshore[MWh]'].map(lambda x: x.replace(',',''))
+    data['Wind offshore[MWh]']= data['Wind offshore[MWh]'].replace('-', np.nan)
+    data['Wind offshore[MWh]']= data['Wind offshore[MWh]'].astype(float)
+
+#data['Wind onshore[MWh]']=data['Wind onshore[MWh]'].map(lambda x : x.replace('.',''))
+#data['Wind onshore[MWh]']=data['Wind onshore[MWh]'].map(lambda x: x.replace(',','.'))
+if not data['Wind onshore[MWh]'].dtype==float:
+    data['Wind onshore[MWh]']=data['Wind onshore[MWh]'].map(lambda x: x.replace(',',''))
+    data['Wind onshore[MWh]']= data['Wind onshore[MWh]'].replace('-', np.nan)
+    data['Wind onshore[MWh]']= data['Wind onshore[MWh]'].astype(float)
+
+#data['Photovoltaics[MWh]']=data['Photovoltaics[MWh]'].map(lambda x : x.replace('.',''))
+#data['Photovoltaics[MWh]']=data['Photovoltaics[MWh]'].map(lambda x: x.replace(',','.'))
+if not data['Photovoltaics[MWh]'].dtype==float:
+    data['Photovoltaics[MWh]']=data['Photovoltaics[MWh]'].map(lambda x: x.replace(',',''))
+    data['Photovoltaics[MWh]']= data['Photovoltaics[MWh]'].replace('-', np.nan)
+    data['Photovoltaics[MWh]']= data['Photovoltaics[MWh]'].astype(float)
 
 group= data.groupby(by=[data.Date,data.Time],as_index=False).sum()
-group.to_excel(r'C:\Thesis\Input Resources\Excel\generation_germany_2016.xlsx', sheet_name='generation-2016')
+group['PV']= group['Photovoltaics[MWh]']/PV_2019
+group['Hydro']= group['Hydropower[MWh]']/Hydro_2019
+group['wind_on']= group['Wind onshore[MWh]']/Wind_on_2019
+group['wind_off']= group['Wind offshore[MWh]']/Wind_off_2019
+print('PV (h):',group['PV'].sum())
+print('Hydro (h):',group['Hydro'].sum())
+print('Wind_off (h):',group['wind_off'].sum())
+print('wind on (h):',group['wind_on'].sum())
+group.to_excel(r'C:\Thesis\Input Resources\Excel\flh_germany_2019.xlsx', sheet_name='renewables_DE_19')
 
 # data.replace(r'-', np.nan, regex=True)
 #data['Time']=data['Time'].apply(lambda x: datetime.strptime(x, '%H:%M').hour)
--- nopt/hourly_deman_data.py
+++ nopt/hourly_deman_data.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import datetime
 from datetime import datetime
-data= pd.read_csv(r'C:\Thesis\Input Resources\Realisierter_Stromverbrauch_201901010000_202001012345_1.csv',sep=';')
+
+data= pd.read_csv(r'C:\Thesis\Input Resources\Excel\Realisierter_Stromverbrauch_201901010000_202001012345_1.csv',sep=';')
 data.columns=['Date','Time','Load']
 data['Time']=data['Time'].apply(lambda x: datetime.strptime(x, '%H:%M').hour)
 data['Date']=data['Date'].apply(lambda x: datetime.strptime(x, '%d.%m.%Y').date())
@@ -11,6 +12,7 @@ data['Load']=data.Load.map(lambda x: x.replace(',','.'))
 data['Load']= data.Load.replace('-', np.nan)
 data.Load= data.Load.astype(float)
 group= data.groupby(by=[data.Date,data.Time],as_index=False).sum()
+
 group.to_excel(r'C:\Thesis\Input Resources\2019_demand_germany.xlsx', sheet_name='Demand')
 
 # data.replace(r'-', np.nan, regex=True)
